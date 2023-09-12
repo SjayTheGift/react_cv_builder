a141149e3e59a64c3f9133ee593295634250b26e
Esethu <jonas.esethu24@gmail.com>
2023-09-12 16:34:47 +0200

    add experience and education

--- src/features/otherInfo/otherInfoBuilderActions.jsx
+++ src/features/otherInfo/otherInfoBuilderActions.jsx
@@ -48,6 +48,7 @@ export const addExperience = createAsyncThunk('builder/addExperience', async (da
     return await axios.post(`${backendURL}/api/cv-builder/experience/`, data, config)
     .then(res => {
         // localStorage.setItem('departmentData', JSON.stringify(res.data)
+        toast.success('Added successfully')
         return res.data
     })
     .catch(error => {
@@ -59,7 +60,7 @@ export const addExperience = createAsyncThunk('builder/addExperience', async (da
 export const updateExperience = createAsyncThunk('builder/updateExperience', async (data, thunkAPI) => {
     return await axios.put(`${backendURL}/api/cv-builder/experience/${data.id}/`,  data , config)
     .then(res => {
-        toast.success(res.data.name + ' Updated')
+        toast.success('Updated successfully')
         return res.data
     })
     .catch(error => {
@@ -72,37 +73,78 @@ export const updateExperience = createAsyncThunk('builder/updateExperience', asy
 
 export const fetchEducation = createAsyncThunk('builder/fetchEducation', async (thunkAPI) => {
 
-    return await axios.get(`${backendURL}/api/cv-builder/education/`, config)
-    .then(res => {
-        localStorage.setItem('experienceInfoData', JSON.stringify(res.data))
-        return res.data
-    })
-    .catch(error => {
-      console.log(error)
-      toast.error(error.message)
-    })
+  return await axios.get(`${backendURL}/api/cv-builder/education/`, config)
+  .then(res => {
+      localStorage.setItem('educationInfoData', JSON.stringify(res.data))
+      return res.data
+  })
+  .catch(error => {
+    console.log(error)
+    toast.error(error.message)
+  })
 })
 
-export const addEducation = createAsyncThunk('builder/addEducation', async (data, thunkAPI) => {
-    console.log(data)
-    return await axios.post(`${backendURL}/api/cv-builder/education/`, data, config)
-    .then(res => {
-        // localStorage.setItem('departmentData', JSON.stringify(res.data)
-        return res.data
-    })
-    .catch(error => {
-      toast.error(error.response.data.name[0])
-    })
-  }
+export const addEducation= createAsyncThunk('builder/addEducation', async (data, thunkAPI) => {
+  return await axios.post(`${backendURL}/api/cv-builder/education/`, data, config)
+  .then(res => {
+      // localStorage.setItem('departmentData', JSON.stringify(res.data)
+      toast.success('Added successfully')
+      return res.data
+  })
+  .catch(error => {
+    toast.error(error.response.data.name[0])
+  })
+}
 )
 
 export const updateEducation = createAsyncThunk('builder/updateEducation', async (data, thunkAPI) => {
-    return await axios.put(`${backendURL}/api/cv-builder/education/${data.id}/`,  data , config)
-    .then(res => {
-        toast.success('Updated Education Data')
-        return res.data
-    })
-    .catch(error => {
-      toast.error(error.response.data.name[0])
-    })
+  return await axios.put(`${backendURL}/api/cv-builder/education/${data.id}/`,  data , config)
+  .then(res => {
+      toast.success('Updated successfully')
+      return res.data
+  })
+  .catch(error => {
+    toast.error(error.response.data.name[0])
+  })
 })
+
+
+
+// Skill CRUD 
+
+// export const fetchSkill = createAsyncThunk('builder/fetchSkill', async (thunkAPI) => {
+
+//   return await axios.get(`${backendURL}/api/cv-builder/skill/`, config)
+//   .then(res => {
+//       localStorage.setItem('skillsData', JSON.stringify(res.data))
+//       return res.data
+//   })
+//   .catch(error => {
+//     console.log(error)
+//     toast.error(error.message)
+//   })
+// })
+
+// export const addSkill = createAsyncThunk('builder/addSkill', async (data, thunkAPI) => {
+//   return await axios.post(`${backendURL}/api/cv-builder/skill/`, data, config)
+//   .then(res => {
+//       // localStorage.setItem('departmentData', JSON.stringify(res.data)
+//       toast.success('Added successfully')
+//       return res.data
+//   })
+//   .catch(error => {
+//     toast.error(error.response.data.name[0])
+//   })
+// }
+// )
+
+// export const updateSkill = createAsyncThunk('builder/updateSkill', async (data, thunkAPI) => {
+//   return await axios.put(`${backendURL}/api/cv-builder/skill/${data.id}/`,  data , config)
+//   .then(res => {
+//       toast.success('Updated successfully')
+//       return res.data
+//   })
+//   .catch(error => {
+//     toast.error(error.response.data.name[0])
+//   })
+// })
